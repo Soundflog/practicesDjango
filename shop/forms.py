2340950931678b2e52c6fd4c48c3a141d1815d95
Soundflog <paha-vershinin@mail.ru>
2024-09-29 22:24:36 +0300

    practice #10
    whithout theorytic

---
+++ shop/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'description', 'category', 'brand', 'image']  # Поля формы
+
+    # Кастомизация формы, добавление классов и валидации
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise forms.ValidationError('Цена должна быть больше нуля.')
+        return price
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label='', max_length=100, required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Название',
+        'class': 'form-control',
+    }))
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db import models
+from shop.forms import ProductForm, SearchForm
 from shop.models import Product
 
 
@@ -14,9 +15,9 @@ def about(request):
 
 
 # Страница товаров
-def products(request):
-    products_list = Product.objects.all()  # Получаем все товары из базы данных
-    return render(request, 'products.html', {'products': products_list})
+# def products(request):
+#     products_list = Product.objects.all()  # Получаем все товары из базы данных
+#     return render(request, 'products.html', {'products': products_list})
 
 
 # Страница с деталями товара
@@ -26,3 +27,42 @@ def product_detail(request, slug):
     return render(request, 'product_detail.html', {'product': product})
 
 
+# Добавление нового товара
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('products')  # Перенаправляем на страницу со списком товаров
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
+
+
+# Редактирование товара
+def edit_product(request, slug):
+    product = Product.objects.get(slug=slug)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'edit_product.html', {'form': form, 'product': product})
+
+
+def products(request):
+    form = SearchForm(request.GET or None)  # Инициализация формы поиска
+    products_list = Product.objects.all()
+
+    # Проверяем, был ли запрос
+    if form.is_valid():
+        query = form.cleaned_data.get('query')
+        if query:
+            # Фильтруем товары по названию или описанию
+            products_list = products_list.filter(
+                models.Q(name__icontains=query) | models.Q(description__icontains=query)
+            )
+
+    return render(request, 'products.html', {'products': products_list, 'form': form})
