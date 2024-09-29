c429db6b83d3beeea2aef6365d985de2b6c94e65
Soundflog <paha-vershinin@mail.ru>
2024-09-29 13:12:05 +0300

    practice 3 Создание моделей

--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.name
