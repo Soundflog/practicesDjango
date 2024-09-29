62ef77271954e9b26b9a28efe9f5df143f09bc74
Soundflog <paha-vershinin@mail.ru>
2024-09-29 14:27:13 +0300

    practice 6 ORM Django response
    Создана функция для отображения книг одного автора через связь «один ко многим» (ForeignKey).
    Создана функция для отображения книг, связанных с одним издателем через связь «многие ко многим» (ManyToManyField).
    Настроены шаблоны для отображения данных и маршруты для работы с запросами.

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.book_detail, name='book_detail'),  # маршрут для отображения книги
+    # path('author/<str:author_name>/', views.books_by_author, name='books_by_author'),
+    # Маршрут для книг автора
+    path('author/<int:author_id>/', views.books_by_author, name='books_by_author'),
+
+    # Маршрут для книг издателя
+    path('publisher/<int:publisher_id>/', views.books_by_publisher, name='books_by_publisher'),
+
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Author, Publisher
 
 
 def book_detail(request):
     book = Book.objects.all()  # Получаем первую книгу из базы данных
     return render(request, 'book_detail.html', {'book': book})
+
+# from .models import Book, Author
+#
+# # Получаем все книги определённого автора
+# def books_by_author(request, author_name):
+#     # Фильтруем книги по имени автора
+#     books = Book.objects.filter(author__name=author_name)
+#     return render(request, 'books_by_author.html', {'books': books, 'author_name': author_name})
+
+# Функция для отображения всех книг одного автора
+def books_by_author(request, author_id):
+    author = Author.objects.get(id=author_id)  # Получаем автора по его ID
+    books = Book.objects.filter(author=author)  # Фильтруем книги по автору
+    return render(request, 'books_by_author.html', {'author': author, 'books': books})
+
+
+# Функция для отображения всех книг, связанных с одним издателем
+def books_by_publisher(request, publisher_id):
+    publisher = Publisher.objects.get(id=publisher_id)  # Получаем издателя по его ID
+    books = publisher.book_set.all()  # Получаем все книги, связанные с этим издателем
+    return render(request, 'books_by_publisher.html', {'publisher': publisher, 'books': books})
