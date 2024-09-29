9810f5f7e4b42744990b5f941f5750ae2ebdf807
Soundflog <paha-vershinin@mail.ru>
2024-09-29 14:08:39 +0300

    practice 5 register in admin panel

--- university/admin.py
+++ university/admin.py
@@ -2,17 +2,23 @@ from django.contrib import admin
 from .models import Student, Teacher, Course
 
 
+# Настройка отображения модели Student
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'age')
+    search_fields = ('name',)  # Добавляем поиск по имени студента
 
 
+# Настройка отображения модели Teacher
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)  # Добавляем поиск по имени учителя
 
 
+# Настройка отображения модели Course
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'teacher')
-    filter_horizontal = ('students',)  # Удобный выбор студентов
+    list_display = ('title', 'teacher')  # Отображаем название курса и учителя
+    search_fields = ('title', 'teacher__name')  # Добавляем поиск по названию курса и имени учителя
+    filter_horizontal = ('students',)  # Для удобного выбора студентов
