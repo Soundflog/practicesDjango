a3fb10d0848089604ccf55f7041f70d747d7f809
Soundflog <paha-vershinin@mail.ru>
2024-10-02 00:16:18 +0300

    улучшен стиль add_product

--- shop/forms.py
+++ shop/forms.py
@@ -7,6 +7,21 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'description', 'category', 'brand', 'image']  # Поля формы
+        labels = {
+            'name': 'Название товара',
+            'price': 'Стоимость',
+            'description': 'Описание товара',
+            'category': 'Категория',
+            'brand': 'Бренд',
+            'image': 'Картинка/Фото',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'min': '1', 'max': '1000000000', 'step': '1.00'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'brand': forms.Select(attrs={'class': 'form-control'})
+        }
 
     # Кастомизация формы, добавление классов и валидации
     def clean_price(self):
@@ -19,6 +34,6 @@ class ProductForm(forms.ModelForm):
 class SearchForm(forms.Form):
     query = forms.CharField(label='', max_length=100, required=False, widget=forms.TextInput(attrs={
         'placeholder': 'Название',
-        'class': 'form-control',
+        'class': 'form-control search_query',
     }))
 
